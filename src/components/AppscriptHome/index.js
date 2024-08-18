9a9b329c43ef6340b2542ceb296f77652b8e6908
moulali <moulalimoula190@gmail.com>
2024-08-18 08:25:03 +0530

    initialising appscript app component

---
+++ src/components/AppscriptHome/index.js
@@ -0,0 +1,12 @@
+import { Component } from "react"
+
+class AppscriptHome extends Component{
+    render(){
+        return(
+            <div>
+                <h1>hello AppScript</h1>
+            </div>
+        )
+    }
+}
+export default AppscriptHome
